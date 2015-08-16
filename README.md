231232154a52981c732ae375fb9267dc6311692e
eg <blackorangebox@gmail.com>
2015-08-16 12:25:23 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Test data generator
+
+This is command line tool test data generator project based on online solution http://www.convertcsv.com/generate-test-data.htm.
+
+When you generate test data online at http://www.convertcsv.com/generate-test-data.htm your browser can only generate up to 50-100K rows, then it hangs. This tool is a good alternative if you want to generate MILLIONS test data rows, using the same definition syntax as used in online web service.
+
+##Usage:
+
+node generator.js COLUMNS_DEFINITION NUMBER_OF_ROWS OUT_FILE
+COLUMNS_DEFINITION - columns definition from http://www.convertcsv.com/generate-test-data.htm (or see below "Allowed Keywords")
+NUMBER_OF_ROWS - number of rows to generate
+OUT_FILE - output file path
+
+###Usage example:
+
+node generator.js "email,first,last,age,street,city,state,zip,digid,date(3),latitude,longitude,pick(RED|BLUE|YELLOW|GREEN|WHITE),string" 10000 out1.csv
+
+
+##Allowed Keywords
+
+Keyword:			Description
+age:			person's age 1 to 120
+alpha
+alpha(n):			string of letters a to z mixed case
+and if n specified then exactly n length long, i.e. alpha(5)
+birthday:			date of birth in mm/dd/yyyy format or birthday(2) for dd/mm/yyyy
+bool:			true or false
+char:			1 single character of a letter or digits or !@#$%^&*()
+city:			cities
+ccnumber:			Credit Card Number
+date
+date(2)
+or date(3)
+or date(4):			date in mm/dd/yyyy format or
+date(2) for dd/mm/yyyy 
+or date(3) for yyyy/mm/dd 
+or date(4) for yyyymmdd
+digit
+digit(n):			digit - 5-20 digits
+digits(n) where n is a number will generate a string exactly that long, i.e. digit(5) 
+See also: integer,float,natural, seq
+dollar:			Dollar amount in format of $99999.99
+domain:			domain name
+email:			dummy email address
+first:			First name
+float:			Floating/Real number with at most 4 digits to right of decimal 
+See also: integer,natural,seq,digit
+gender:			Male or Female
+integer:			Integer value - includes negatives. 
+See also: natural,float,seq,digit
+last:			Last name
+latitude:			Latitude
+longitude:			Longitude
+mi:			middle initial - 1 letter
+name:			full person's name
+natural
+natural(n):			Integer value 0 or above, or
+natural(n) where n is maximum integer value 
+See also: integer,float,seq,digit
+paragraph:			paragraph of words - 3 to 7 sentences
+phone:			US phone number 999-999-9999
+pick(pick1|pick2|...):			choose one of choices separated by |
+postal:			Canadian postal (see also province)
+province:			Canadian pronvince (see also postal)
+seq
+seq(n):			numeric integer sequence starting at 1 or n and incremented by 1 
+seq(n) - sequence starts at integer n, i.e. seq(1000)
+sentence:			sentence words - 12 to 18 words
+state:			state
+street:			street address
+string
+string(n):			random string value of letters,digits, and !@#$%^&*()
+string(n) where n is length.
+word:			words - 5-6 letters
+yn:			Y or N
+zip:			5 digit US zip code
+zip9:			9 digit US zip code in 99999-9999 format
---
+++ generator.js
@@ -0,0 +1,242 @@
+var fs = require('fs');
+var chanceModule = require('./lib/chance');
+eval(fs.readFileSync('lib/csvsup.js')+'');
+eval(fs.readFileSync('lib/strsup.js')+'');
+
+var chance = new chanceModule.Chance();
+var CSV = {delimiter: ","};
+
+// Taken from http://www.convertcsv.com/generate-test-data.htm
+function genData(columns, rows)
+{
+    var j;
+    var cols = columns.length;
+    var s = "";
+    var fld ="";
+    var n=0;
+    var t = "";
+    
+    hdr = [];
+
+    rows = rows*1 || 100;
+    for(k=0;k<cols;k++) { // for each field, determine heading and options
+       fld = columns[k].split(/[()]/);
+       if(fld.length>1)n=fld[1];
+       hdr.push({seqObj: null,keyword: columns[k].split('(')[0].toLowerCase().trim(),arg: (fld.length>1) ? fld[1] : "" });
+       if(hdr[hdr.length-1].keyword=="seq") {
+           if(fld.length>1 && !isNaN(hdr[hdr.length-1].arg)) {
+               //alert('seq set at '+(hdr.length-1)+", and starting n at "+ hdr[hdr.length-1].arg);
+               hdr[hdr.length-1].seqObj = new SeqObj(hdr[hdr.length-1].arg);
+           } else {
+               hdr[hdr.length-1].seqObj = new SeqObj(); 
+               //alert('seq set at '+hdr.length-1);
+           }
+           //seqobj.n=hdr[k].arg-1;
+       }
+    }
+    for (j=1;j<=rows;j++) {  // for each row
+        for(k=0;k<cols;k++) { // for each field, generate data cell
+            switch(hdr[k].keyword) {
+                case 'age' : 
+                   s += chance.age();
+                   break;
+                case 'alpha' :   //alpha or alpha(n)
+                   n=hdr[k].arg;
+                   if(n!="") {
+                      s += chance.string({pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', length: n}); 
+                   }
+                   else {
+                      s += chance.string({pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'}); 
+                   }
+                   break;
+                case 'birthday' :
+                   n=hdr[k].arg;
+                   switch(n) {
+                      case '2' : s += chance.birthday({string: true, american: false}).toCsv(CSV.delimiter);break;
+                      default: s += chance.birthday({string: true}).toCsv(CSV.delimiter);break;
+                   }
+                   break;
+                case 'bool' :   
+                   s += chance.bool();
+                   break;
+                case 'city' : 
+                   s += chance.city().toCsv(CSV.delimiter);
+                   break;
+                case 'ccnumber' : 
+                   s += chance.cc().toCsv(CSV.delimiter);
+                   break;
+                case 'char' : // one character
+                   s += chance.character();
+                   break;
+                case 'date' :
+                   n=hdr[k].arg;
+                   switch(n) {
+                      case '2' : s += chance.date({string: true, american: false}).toCsv(CSV.delimiter);break;
+                      case '3' : t = chance.date();
+                               t = t.getFullYear() + '/' + ("0" + (t.getMonth() + 1)).slice(-2) + '/' + ("0" + t.getDate()).slice(-2);
+                               s += t;
+                               break;
+                      case '4' : t = chance.date();
+                               t = t.getFullYear() + ("0" + (t.getMonth() + 1)).slice(-2) + ("0" + t.getDate()).slice(-2);
+                               s += t;
+                               break;
+                      default: s += chance.date({string: true}).toCsv(CSV.delimiter);break;
+                   }
+                   break;
+                   
+                case 'digit' :
+                   n=hdr[k].arg;
+                   if(n!="" && n>0) {
+                      s += chance.string({pool: '0123456789', length: n}); 
+                   }
+                   else {
+                      s += chance.string({pool: '0123456789'}); 
+                   }
+                   break;
+                case 'dollar' :
+                   s += chance.dollar().toCsv(CSV.delimiter);
+                   break;
+                case 'domain' :
+                   s += chance.domain().toCsv(CSV.delimiter);
+                   break;
+                case 'email' :
+                   s += chance.email().toCsv(CSV.delimiter);
+                   break;
+                case 'first' : 
+                   s += chance.first().toCsv(CSV.delimiter);
+                   break;
+                case 'float' : 
+                   s += chance.floating();
+                   break;
+                case 'gender' :
+                   s += chance.gender();
+                   break;
+                case 'integer' : 
+                   s += chance.integer({min: -999999, max: 999999});
+                   break;
+                case 'last' : 
+                   s += chance.last().toCsv(CSV.delimiter);
+                   break;
+
+                case 'latitude' : 
+                   s += chance.latitude();
+                   break;
+                case 'longitude' : 
+                   s += chance.longitude();
+                   break;
+
+                case 'mi' : // middle initial
+                   s += chance.character({alpha: true, casing : 'upper'});
+                   break;
+                case 'name' : 
+                   s += chance.name().toCsv(CSV.delimiter);
+                   break;
+                case 'natural' : 
+                   n=hdr[k].arg;
+                   if(n!="" && (n*1)>=0) {
+                      s += chance.natural({max: n});
+                   }else{
+                      s += chance.natural();
+                   }
+                   break;
+                case 'paragraph' : // argument is max length in characters
+                   n=hdr[k].arg;
+                   if(n!="" && (n*1)>0) {
+                      s += chance.paragraph().left(n).toCsv(CSV.delimiter);
+                   }else{
+                      s += chance.paragraph().toCsv(CSV.delimiter);
+                   }
+                   break;
+                case 'phone' : 
+                   s += chance.phone().toCsv(CSV.delimiter);
+                   break;
+                case 'pick' : 
+                   fld=hdr[k].arg;
+                   s += chance.pick(fld.split('|')).toCsv(CSV.delimiter);
+                   break;
+                case 'postal' : 
+                   s += chance.postal().toCsv(CSV.delimiter);
+                   break;
+                case 'province' : 
+                   s += chance.province().toCsv(CSV.delimiter);
+                   break;
+                case 'sentence' : // argument is max length in characters
+                   n=hdr[k].arg;
+                   if(n!="" && (n*1)>0) {
+                      s += chance.sentence().left(n).toCsv(CSV.delimiter);
+                   }else{
+                      s += chance.sentence().toCsv(CSV.delimiter);
+                   }
+                   break;
+                case 'seq' :
+                   s += hdr[k].seqObj.next();
+                   //s += seqobj.next();
+                   break;
+                case 'state' :
+                   s += chance.state().toCsv(CSV.delimiter);
+                   break;
+                case 'street' :
+                   s += chance.street().toCsv(CSV.delimiter);
+                   break;
+                case 'string' : 
+                   n=hdr[k].arg;
+                   if(n!="" && (n*1)>0) {
+                      s += chance.string({length: n}).toCsv(CSV.delimiter);
+                   }else{
+                      s += chance.string().toCsv(CSV.delimiter);
+                   }
+                   break;
+                case 'word' :
+                   s += chance.word().toCsv(CSV.delimiter);
+                   break;
+                case 'yn' : 
+                   s += chance.character({pool: 'YN'});
+                   break;
+                case 'zip' : 
+                   s += chance.zip(); 
+                   break;
+                case 'zip9' : 
+                   s += chance.zip({plusfour: true});
+                   break;
+                case 'yn' : 
+                   s += chance.character({pool: 'YN'});
+                   break;
+                default:
+                   fld=columns[k];
+                   if(fld=="FIELD"+(k+1))fld="";
+                   s += fld.toCsv(CSV.delimiter);
+                   break;
+
+            }
+            if(k<cols-1) {
+                s+=CSV.delimiter;
+            }
+        }
+        s+="\n";
+    }
+    return s;
+}
+
+var dataRowsRequired = process.argv[3];
+var chunkSize = 1000;
+var columns = process.argv[2].split(',');
+
+console.log('dataRowsRequired', dataRowsRequired);
+console.log('columns', columns);
+
+var outputFileName = process.argv.length > 4? process.argv[4]: 'out.csv';
+if (fs.existsSync(outputFileName)){
+	fs.unlinkSync(outputFileName);
+}
+
+for(var i = 0; i <dataRowsRequired; i = i + chunkSize){
+	var size = chunkSize>dataRowsRequired? dataRowsRequired: chunkSize;
+	var csvStringPart = genData(columns, size);
+	console.log("Appending csv part ", i ," of size ", size);
+	fs.appendFile(outputFileName, csvStringPart, function (err) {
+		if(err) {
+			return console.log(err);
+		}
+		console.log("Appended csv part ", i ," of size ", size);
+	});
+}
